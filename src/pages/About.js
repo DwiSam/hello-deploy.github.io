e45e496dbda72df58dd0803ae8a910c42afad1fd
Dwi Sam <dwisamsiarto@gmail.com>
2022-10-09 22:25:45 +0700

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div className="container pt-4">
+            <h1>Biodata</h1>
+            <hr />
+            <p>Nama : Dwi Samsiarto</p>
+            <p>No. Peserta : RCTN-KS05-023</p>
+            <p>Hobi : Rebahan</p>
+            <p>Progress Aplikasi Persesi</p>
+            <ul>
+                <li>Sesi 1 s.d. 13 sudah/done</li>
+            </ul>
+        </div>
+    );
+};
+
+export default About;
